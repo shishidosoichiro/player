8a29077f424767b2821411fadd9a46353a8e6269
Soichiro Shishido <shishidosoichiro@hotmail.com>
2013-05-06 14:53:20 +0900

    ready to docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,9 +37,45 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'build/<%= pkg.main[0] %>',
+        dest: 'build/<%= pkg.main[1] %>'
       }
+    },
+    copy: {
+    	main: {
+				files: [
+					{
+						expand: true,
+						cwd: 'build/', 
+						src: ['<%= pkg.main[1] %>'],
+						dest: 'docs/js'
+					},
+					{
+						expand: true,
+						cwd: 'components/jquery/', 
+						src: ['jquery.min.js'],
+						dest: 'docs/js/'
+					},
+					{
+						expand: true,
+						cwd: 'components/bootstrap/docs/assets/css', 
+						src: ['**'],
+						dest: 'docs/css/'
+					},
+					{
+						expand: true,
+						cwd: 'components/bootstrap/docs/assets/js', 
+						src: ['bootstrap.min.js', 'html5shiv.js'],
+						dest: 'docs/js/'
+					},
+					{
+						expand: true,
+						cwd: 'components/bootstrap/docs/assets/img', 
+						src: ['glyphicons-halflings-white.png', 'glyphicons-halflings.png'],
+						dest: 'docs/img/'
+					}
+				]
+    	}
     },
     watch: {
 			files: ['<%= jshint.node.files %>', '<%= jshint.client.files %>', '<%= jshint.docs.files %>'],
@@ -52,8 +88,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
 
 };
